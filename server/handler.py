71f305c8597bfd78a64e41f3ca42ced13fc5875f
hxrshdeep <ishu.cxx@gmail.com>
2023-09-22 13:43:14 -0800

    #3333

--- server/handler.py
+++ server/handler.py
@@ -3,9 +3,3 @@ import random
 RED = "\033[91m"
 GREEN = "\033[92m"
 RESET = "\033[0m"
-
-def storage(x):
-    n = f"{random.randint(1, 10000000)}.txt"
-    with open(n, "w") as file:
-        file.write(x)
-    print(GREEN,"[*] Received and saved as : ", n,RESET)
--- server/server.py
+++ server/server.py
@@ -5,35 +5,27 @@ def main():
     x = str(input("[*] Listener IP   :   "))
     y = int(input("[*] Listener PORT :   "))
 
-    try:
-        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        soc.bind((x, y))
-        soc.listen()
-        print(handler.GREEN + "[*] Handler listening ..." + handler.RESET)
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.bind((x, y))
+    soc.listen()
+    print(handler.GREEN + "[*] Handler listening ..." + handler.RESET)
+
+    while True:
+        conn, addr = soc.accept()
+        print(handler.GREEN + "[*] Connected to client:", addr + handler.RESET)
 
         while True:
-            conn, addr = soc.accept()
-            print(handler.GREEN + "[*] Connected to client:", addr + handler.RESET)
+            try:
+                cmd = input(">> ")
+                conn.send(cmd.encode())
+                res = conn.recv(8192).decode()
+                print(res)
 
-            while True:
-                try:
-                    cmd = input(">> ")
-                    conn.send(cmd.encode())
-                    res = conn.recv(8192).decode()
-
-                    if res == "0":
-                        print(handler.RED + "[!] Invalid command !" + handler.RESET)
-                    else:
-                        handler.storage(res)
-
-                except Exception:
-                    conn.close()
-                    print(handler.RED + "[!] Client disconnected {addr}" + handler.RESET)
-                    print(handler.GREEN + "[*] Waiting for clients..." + handler.RESET)
-                    break
-
-    except Exception:
-        print(handler.RED + f"[*] Unexpected error" + handler.RESET)
+            except Exception:
+                conn.close()
+                print(handler.RED + "[!] Client disconnected {addr}" + handler.RESET)
+                print(handler.GREEN + "[*] Waiting for clients..." + handler.RESET)
+                break
 
 if __name__ == "__main__":
     main()
